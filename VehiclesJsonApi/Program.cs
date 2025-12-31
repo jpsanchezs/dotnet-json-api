107255e7dce9407ea2fd1661f319dfba8555181d
jpsanchez <pablosanchez19@estudiantec.cr>
2025-12-31 16:47:50 -0600

    enaleb swagger in production

--- VehiclesJsonApi/Program.cs
+++ VehiclesJsonApi/Program.cs
@@ -1,20 +1,30 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Vehicles API",
+        Version = "v1",
+        Description = "API para manejar datos de vehículos (carros y motos)"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Swagger siempre, no solo en Development
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vehicles API V1");
+    c.RoutePrefix = "swagger"; // URL será /swagger
+});
 
 app.UseHttpsRedirection();
 
