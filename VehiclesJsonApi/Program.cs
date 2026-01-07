46689ae898a1e9ca6fdb2669e11dd246ebe2fa97
jpsanchezs <jpsanchezsa@gmail.com>
2026-01-07 14:28:30 -0600

    Update Program.cs v2.2

--- VehiclesJsonApi/Program.cs
+++ VehiclesJsonApi/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
                 .WithOrigins(
                     "http://localhost:5173",
                     "https://localhost:5173",
-                    "http://localhost:4173"
+                    "http://localhost:4173",
                     "https://vehicle-information-web.onrender.com" 
                 )
                 .AllowAnyHeader()
